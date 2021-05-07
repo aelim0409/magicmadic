d1ec1bb4779c546293f2acfb924e8043cc7b2ce4
aelim0409 <72659915+aelim0409@users.noreply.github.com>
2021-05-07 15:56:26 +0900

    회원가입 후 -> 로그인창 버튼 추가

--- app/src/main/java/com/example/myapplication2/SignUp.java
+++ app/src/main/java/com/example/myapplication2/SignUp.java
@@ -1,13 +1,27 @@
 package com.example.myapplication2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        Button btn_signUp2 = findViewById(R.id.button_signup2);
+        btn_signUp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),login.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
