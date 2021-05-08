2399cfba52666961bba773a7d72410232868a7b5
slcloe <gkstmf1403@naver.com>
2021-05-08 15:59:54 +0900

    add success

--- app/src/main/java/com/example/myapplication2/MainActivity2.java
+++ app/src/main/java/com/example/myapplication2/MainActivity2.java
@@ -65,7 +65,7 @@ public class MainActivity2 extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         TextView todayText = findViewById(R.id.today_text);
         todayText.setOnClickListener(new View.OnClickListener() {
