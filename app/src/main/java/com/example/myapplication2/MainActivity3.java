c92d6a3409a88a1538643462bcb09bae36d4fddd
김수라 <srsw000521@gmail.com>
2021-05-19 17:50:28 +0900

    MainActivity3 수정

--- app/src/main/java/com/example/myapplication2/MainActivity3.java
+++ app/src/main/java/com/example/myapplication2/MainActivity3.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -13,6 +15,40 @@ public class MainActivity3 extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Switch switch1=findViewById(R.id.switch1);
+        Switch switch2=findViewById(R.id.switch2);
+        Switch switch3=findViewById(R.id.switch3);
+        Switch switch4=findViewById(R.id.switch4);
+        Switch switch5=findViewById(R.id.switch5);
+
+        class switch1Listener implements CompoundButton.OnCheckedChangeListener {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    Intent intent = new Intent(getApplicationContext(), pills_mod_reminder.class);
+                    startActivity(intent);
+                }
+            }
+        }
+
+        class switch3Listener implements CompoundButton.OnCheckedChangeListener {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Intent intent = new Intent(getApplicationContext(), HospitalModReminderActivity.class);
+                    startActivity(intent);
+                }
+            }
+        }
+
+        switch1.setOnCheckedChangeListener(new switch1Listener());
+        switch3.setOnCheckedChangeListener(new switch3Listener());
+
+
+
+
+
 
         Button btn_home = findViewById(R.id.home_btn);
         btn_home.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +86,5 @@ public class MainActivity3 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
-
 }
