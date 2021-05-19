a71b1354e2952353d0a22fa0bcdbee0b3faaf0a3
김수라 <srsw000521@gmail.com>
2021-05-19 16:56:03 +0900

    MainActivity3 수정

--- app/src/main/java/com/example/myapplication2/MainActivity3.java
+++ app/src/main/java/com/example/myapplication2/MainActivity3.java
@@ -1,13 +1,56 @@
 package com.example.myapplication2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button btn_home = findViewById(R.id.home_btn);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),basic_information_page.class);
+                startActivity(intent);
+            }
+        });
+
+        Button fitness_btn = findViewById(R.id.fitness_btn);
+        fitness_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),basic_information_page.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button calendar_btn = findViewById(R.id.calendar_btn);
+        calendar_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),basic_information_page.class);
+                startActivity(intent);
+            }
+        });
+
+        Button remind_btn = findViewById(R.id.remind_btn);
+        remind_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),basic_information_page.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
