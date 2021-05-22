4b01bb23181fa676cf2285eb7f923ec3f8cf46ae
김수라 <srsw000521@gmail.com>
2021-05-23 01:49:13 +0900

    MainActivity3 수정

--- app/src/main/java/com/example/myapplication2/MainActivity3.java
+++ app/src/main/java/com/example/myapplication2/MainActivity3.java
@@ -21,6 +21,7 @@ public class MainActivity3 extends AppCompatActivity {
         Switch switch3=findViewById(R.id.switch3);
         Switch switch4=findViewById(R.id.switch4);
         Switch switch5=findViewById(R.id.switch5);
+        Switch switch6=findViewById(R.id.switch6);
 
         class switch1Listener implements CompoundButton.OnCheckedChangeListener {
             @Override
@@ -41,13 +42,40 @@ public class MainActivity3 extends AppCompatActivity {
                 }
             }
         }
+        class switch4Listener implements CompoundButton.OnCheckedChangeListener {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Intent intent = new Intent(getApplicationContext(), water_mod_reminder.class);
+                    startActivity(intent);
+                }
+            }
+        }
+        class switch5Listener implements CompoundButton.OnCheckedChangeListener {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Intent intent = new Intent(getApplicationContext(), exercise_mode_reminder.class);
+                    startActivity(intent);
+                }
+            }
+        }
+        class switch6Listener implements CompoundButton.OnCheckedChangeListener {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Intent intent = new Intent(getApplicationContext(), sleep_mod_reminder.class);
+                    startActivity(intent);
+                }
+            }
+        }
 
         switch1.setOnCheckedChangeListener(new switch1Listener());
+
         switch3.setOnCheckedChangeListener(new switch3Listener());
-
-
-
-
+        switch4.setOnCheckedChangeListener(new switch4Listener());
+        switch5.setOnCheckedChangeListener(new switch5Listener());
+        switch6.setOnCheckedChangeListener(new switch6Listener());
 
 
         Button btn_home = findViewById(R.id.home_btn);
