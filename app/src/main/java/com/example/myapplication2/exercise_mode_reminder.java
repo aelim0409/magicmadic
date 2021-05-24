e279c46224ac1511da128e074803513187c11bd2
aelim0409 <72659915+aelim0409@users.noreply.github.com>
2021-05-24 23:07:12 +0900

    서버 연동 코드 수정 3....

--- app/src/main/java/com/example/myapplication2/exercise_mode_reminder.java
+++ app/src/main/java/com/example/myapplication2/exercise_mode_reminder.java
@@ -19,6 +19,24 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class exercise_mode_reminder extends AppCompatActivity {
+    public String getInformation(String ID) {
+        Log.w("운동 초기 설정", "설정 정보 주는중");
+        String result="null";
+        try {
+            String id = ID;
+
+            Log.w("(초기)앱에서 보낸 값", id );//+water
+            exercise_mode_reminder.getTask task = new exercise_mode_reminder.getTask();
+            result = task.execute(id).get();
+            Log.w("(초기)받은값", result);
+
+            return result;
+
+        } catch (Exception e) {
+
+        }
+       return result;
+    }
 
 
     @Override
@@ -26,11 +44,19 @@ public class exercise_mode_reminder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercise_mode_reminder);
 
+
         Intent Intent = getIntent();
         String ID = Intent.getStringExtra("Id");
 
         Button button_move;
         EditText exercise_time = (EditText) findViewById(R.id.exercise_hour);
+
+        String info =getInformation(ID);
+        String [] init_info = info.split(" ");
+
+
+        exercise_time.setText(init_info[0]);
+
 
         button_move = findViewById(R.id.button_move);
         button_move.setOnClickListener(new View.OnClickListener() {
@@ -123,7 +149,49 @@ public class exercise_mode_reminder extends AppCompatActivity {
             return receiveMsg;
         }
     }
+    class getTask extends AsyncTask<String,Void,String> {
+        String sendMsg,receiveMsg;
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
 
+                URL url = new URL("http://3.36.134.232:8080/MedicMagic_SPRING/getUserReminderList_view");  // 어떤 서버에 요청할지(localhost 안됨.)
+                // ex) http://123.456.789.10:8080/hello/android
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");                              //데이터를 POST 방식으로 전송합니다.
+                conn.setDoOutput(true);
+
+                // 서버에 보낼 값 포함해 요청함.
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "id="+strings[0];
+                // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
+                osw.write(sendMsg);                           // OutputStreamWriter에 담아 전송
+                osw.flush();
+
+
+                // jsp와 통신이 잘 되고, 서버에서 보낸 값 받음.
+                if(conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+                } else {    // 통신이 실패한 이유를 찍기위한 로그
+                    Log.i("통신 결과", conn.getResponseCode()+"에러");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 서버에서 보낸 값을 리턴합니다.
+            return receiveMsg;
+        }
+    }
 }
 
 
